2a114daa8042c777fd75323363043cdecfb703b7
Jonathan Laferen <60574415+jonathanmarp@users.noreply.github.com>
2023-05-19 21:44:36 +0700

    Buat Page Home, Responsive untuk Smartphone View dan Juga Computer View

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,19 @@
-import Link from "next/link"
-
 import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
 
 export default function IndexPage() {
   return (
     <section>
       {/* Home Page */}
       <div className="w-[100vw] h-[90vh] flex items-center justify-center" id="home">
-        <div className="w-[80%] h-[90%] flex justify-center items-center">
+        <div className="w-[100%] md:w-[80%] h-[90%] relative md:flex md:justify-center md:items-center">
           {/* Image */}
-          <div className="w-[30%] h-[70%] bg-gradient-to-r from-gray-800 from-10%">
-            
-          </div>
+          <div className={cn(
+            "absolute bottom-0 md:relative md:flex w-[100%] md:w-[30%] h-[70%] bg-gradient-to-t",
+            "md:bg-gradient-to-r from-gray-800 from-10%")}></div>
 
           {/* Text */}
-          <div className="h-[70%] w-[60%]">
+          <div className="h-[70%] md:w-[60%] w-[90%]">
             <div className="px-10 py-5">
               <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
                 Something !
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,28 +1,67 @@
+import * as React from "react"
+
 import Link from "next/link"
 
+import {
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
 import { Button } from "@/components/ui/button"
 
 export function SiteHeader() {
   return (
-    <header className="w-full p-7 px-14 flex justify-between">
+    <header className="w-full p-7 sm:px-14 px-7 flex justify-between">
       <div>
         <h1 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
           <span className="font-extrabold">Project</span> FT
         </h1>
       </div>
 
-      <menu className="flex items-center space-x-1 gap-6">
+      {/* Menu */}
+      <Menubar className="sm:hidden flex">
+        <MenubarMenu>
+          <MenubarTrigger>Menu</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              Home <MenubarShortcut>#1</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>
+              About <MenubarShortcut>#2</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>
+              Contact <MenubarShortcut>#3</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>
+              Achivement <MenubarShortcut>#4</MenubarShortcut>
+            </MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
+      
+      {/* Menu If Large */}
+      <menu className="hidden sm:flex items-center space-x-1 gap-6">
         <div className="text-lg font-semibold border-b-2">
-          <Link href="/">Home</Link>
+          <Link href="/#home">Home</Link>
         </div>
         <div className="text-lg font-semibold">
-          <Link href="/">About</Link>
+          <Link href="/#about">About</Link>
         </div>
         <div className="text-lg font-semibold">
-          <Link href="/">Contact</Link>
+          <Link href="/#contact">Contact</Link>
         </div>
         <Button className="text-lg font-semibold">
-          <Link href="/">Achivement</Link>
+          <Link href="/#achivement">Achivement</Link>
         </Button>
       </menu>
     </header>
